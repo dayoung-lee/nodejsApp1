b1891368d4a555dcc114bfec0c4b5f888cad9ef6
Dayoung Lee <55811738+dayoung-lee@users.noreply.github.com>
2020-01-23 13:52:40 -0500

    update main.js

--- main.js
+++ main.js
@@ -132,7 +132,7 @@ var app = http.createServer(function(request,response){
         );
         response.writeHead(200);
         response.end(html); 
-        }); // readdir
+        }); 
     }else if(pathname === '/create_process'){
         var body = '';
         request.on('data', function(data){            
